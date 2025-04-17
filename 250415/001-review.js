24e4ef2610465a336ed4217bc25f29c521c36b9c
김현미 <hyunmi6659@gmail.com>
2025-04-17 17:11:26 +0900

    📝 Update : 250415

---
+++ 250415/001-review.js
@@ -0,0 +1,37 @@
+// AJAX
+// Fetch API
+// Async/Await
+
+// Fetch API
+// fetch("https://dev.wenivops.co.kr/services/fastapi-crud/1/blog")
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then((json) => {
+//     console.log(json);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+//async await
+async function getBlog() {
+    const response = await fetch(
+        "https://dev.wenivops.co.kr/services/fastapi-crud/1/blog"
+    );
+    if(!response.ok){
+        throw new Error("오류 발생");
+    }
+    return response.json();
+}
+async function main() {
+    // 오류 처리 try-catch
+    try{
+        const blogs = await getBlog();
+        console.log(blogs);
+    }   catch (error) {
+        console.log("오류 발생 :", error);
+    }
+}
+main();
